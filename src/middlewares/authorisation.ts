190b9f9e3e44dddf54339a84873319d7bcb53607
Shailesh Singh <shailesh7360098753@gmail.com>
2026-06-29 07:57:45 +0530

    final auth files

---
+++ src/middlewares/authorisation.ts
@@ -0,0 +1,59 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../db/userSchema.js';
+
+
+export interface Authrequest extends Request {
+    user?: any;
+}
+
+interface JwtPayload {
+    id: string;
+}
+
+type Routehandler = (
+    req: Authrequest,
+    res: Response,
+    next: NextFunction,
+) => Promise<void>;
+
+export const protect: Routehandler = async (req, res, next) => {
+    let token: string | undefined;
+
+    if (req.header('authorization') && req.headers.authorization?.startsWith('Bearer')) {
+        token = req.headers.authorization.split('')[1];
+    }
+
+    if (!token) {
+        res.status(401).json({
+            success: false,
+            message: "Not auhtorised , no token",
+        })
+        return;
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+
+        const user = await User.findById(decoded.id).select('-password');
+
+        if (!user) {
+            res.status(401).json({
+                success: false,
+                message: 'User not found',
+            })
+            return;
+        }
+
+        req.user = user;
+
+        next();
+    } catch (error: any) {
+        res.status(401).json({
+            success: false,
+            message: 'Not Authorized , token failed',
+        });
+
+        next(error);
+    }
+}
